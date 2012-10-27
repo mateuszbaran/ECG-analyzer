14274c9fba79f515d7780646caa09a9c6b329f02
Tomasz Drzewiecki <kn65op@gmail.com>
2012-10-27 22:30:45 +0200

    Add modules fields in controlelr

--- ECGController.h
+++ ECGController.h
@@ -24,16 +24,29 @@ public:
   ~ECGController (void);
 
 private:
+  //data
   ECGSignal raw_signal;
   ECGSignal filtered_signal;
-  ECGRs r_peaks;
-  ECGWaves waves;
-  ECGHRV1 hrv1;
-  ECGHRV2 hrv2;
-  ECGHRVDFA ecghrvdfa;
-  ECGClasses ecgclasses;
-  ECGST ecgst;
-  ECGTWave ecgtwave;
-  ECGHRT ecghrt;
+  ECGRs r_peaks_data;
+  ECGWaves waves_data;
+  ECGHRV1 hrv1_data;
+  ECGHRV2 hrv2_data;
+  ECGHRVDFA ecghrvdfa_data;
+  ECGClasses ecgclasses_data;
+  ECGST ecgst_data;
+  ECGTWave ecgtwave_data;
+  ECGHRT ecghrt_data;
+
+  //modules
+  ECGBaselineModule *ecg_baseline_module;
+  RPeaksModule *rpeaks_module;
+  WavesModule *waves_module;
+  HRV1Module *hrv1_module;
+  HRV2Module *hrv2_module;
+  HRVDFAModule *hrv_dfa_module;
+  QRSClassModule *qrs_class_module;
+  STIntervalModule *st_interval_module;
+  TWaveAltModule *t_wave_alt_module;
+  HRT *hrt_module;
 };
 
