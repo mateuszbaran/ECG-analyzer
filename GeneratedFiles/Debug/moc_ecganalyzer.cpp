626515beb35c87410db855b396b20fcfa5e3f694
lpekala <lukasz.pek@gmail.com>
2012-10-27 13:49:56 +0200

    (+) Ikony GUI (+) Menu (+) Kilka Akcji

--- GeneratedFiles/Debug/moc_ecganalyzer.cpp
+++ GeneratedFiles/Debug/moc_ecganalyzer.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'ecganalyzer.h'
 **
-** Created: Thu 18. Oct 18:59:13 2012
+** Created: Sat 27. Oct 13:44:42 2012
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.3)
 **
 ** WARNING! All changes made in this file will be lost!
@@ -23,25 +23,39 @@ static const uint qt_meta_data_ECGanalyzer[] = {
        6,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: signature, parameters, type, tag, flags
+      13,   12,   12,   12, 0x08,
+      39,   12,   12,   12, 0x08,
+      72,   12,   12,   12, 0x08,
+
        0        // eod
 };
 
 static const char qt_meta_stringdata_ECGanalyzer[] = {
-    "ECGanalyzer\0"
+    "ECGanalyzer\0\0on_actionO_Qt_triggered()\0"
+    "on_actionO_Programie_triggered()\0"
+    "on_actionWyjd_triggered()\0"
 };
 
 void ECGanalyzer::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Q_ASSERT(staticMetaObject.cast(_o));
+        ECGanalyzer *_t = static_cast<ECGanalyzer *>(_o);
+        switch (_id) {
+        case 0: _t->on_actionO_Qt_triggered(); break;
+        case 1: _t->on_actionO_Programie_triggered(); break;
+        case 2: _t->on_actionWyjd_triggered(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -76,6 +90,11 @@ int ECGanalyzer::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     _id = QMainWindow::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 3)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 3;
+    }
     return _id;
 }
 QT_END_MOC_NAMESPACE
--- ecganalyzer.h
+++ ecganalyzer.h
@@ -12,6 +12,13 @@ public:
 	ECGanalyzer(QWidget *parent = 0, Qt::WFlags flags = 0);
 	~ECGanalyzer();
 
+private slots:
+    void on_actionO_Qt_triggered();
+
+    void on_actionO_Programie_triggered();
+
+    void on_actionWyjd_triggered();
+
 private:
 	Ui::ECGanalyzerClass ui;
 };
