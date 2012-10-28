8ddc0b569da08ded3edbc8897094d50fb2d48bb3
Tomasz Drzewiecki <kn65op@gmail.com>
2012-10-28 15:34:10 +0100

    Make Gtest running

---
+++ ControllerTest/main.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+
+#include "ECGController.h"
+
+class ECGControllerTest : public ::testing::Test
+{
+protected:
+
+  ECGController controller;
+};
+
+TEST_F(ECGControllerTest, ReadFileValidTest)
+{
+  EXPECT_EQ(false, controller.readFile("no_file_QWE"));
+}
+
+TEST_F(ECGControllerTest, ReadFileInvalidTest)
+{
+  EXPECT_EQ(true, controller.readFile("100"));
+}
+
+int main(int argc, char** argv) 
+{ 
+    testing::InitGoogleTest(&argc, argv); 
+    RUN_ALL_TESTS(); 
+    std::getchar(); // keep console window open until Return keystroke
+}
