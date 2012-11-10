bd47ff1cc51a0f1a133943ca5adfd6c88a3d927a
Norbert Pabian <norbert@FukoVaio.(none)>
2012-11-10 17:00:52 +0100

    Update R-peaks basic skeleton

--- ECGRs.cpp
+++ ECGRs.cpp
@@ -1,70 +1,7 @@
 #include "ECGRs.h"
 
 ECGRs::ECGRs ()
-{
-	rsDetected = false;
-	detectionMethod = PAN_TOMPKINS;
-}
-
-ECGRs::ECGRs (IntSignal filteredSignal)
-{
-	rsDetected = false;
-	filteredSignal = filteredSignal;
-	detectionMethod = PAN_TOMPKINS;
-	detectRPeaks();
-}
-
-ECGRs::ECGRs (IntSignal filteredSignal, R_PEAKS_DETECTION_METHOD detectionMethod)
-{
-	rsDetected = false;
-	filteredSignal = filteredSignal;
-	detectionMethod = detectionMethod;
-	detectRPeaks();
-}
+{}
 
 ECGRs::~ECGRs (void)
 {}
-
-bool ECGRs::areRsDetected()
-{
-	return rsDetected;
-}
-
-void ECGRs::setFilteredSignal(IntSignal filteredSignal)
-{  
-	filteredSignal = filteredSignal;
-	rsDetected = false;
-}
-
-IntSignal ECGRs::getFilteredSignal()
-{
-	return filteredSignal;
-}
-
-void ECGRs::setDetectionMethod(R_PEAKS_DETECTION_METHOD detectionMethod)
-{
-	detectionMethod = detectionMethod;
-}
-
-R_PEAKS_DETECTION_METHOD ECGRs::getDetectionMethod()
-{
-	return detectionMethod;
-}
-
-bool ECGRs::detectRPeaks()
-{
-	if(detectionMethod == PAN_TOMPKINS)
-		panTompkinsRPeaksDetection(&filteredSignal);
-	else if (detectionMethod == HILBERT)
-		hilbertRPeaksDetection(&filteredSignal);
-}
-
-void ECGRs::panTompkinsRPeaksDetection(IntSignal *signal)
-{
-	rsDetected = true;
-}
-
-void ECGRs::hilbertRPeaksDetection(IntSignal *signal)
-{
-	rsDetected = true;
-}
--- ECGRs.h
+++ ECGRs.h
@@ -4,115 +4,23 @@
 #include "ModulesMethods.h"
 
 /**
- * Class ECGRs provides set of method to detect R peaks in ECG signal.
  * @class R peaks.
  */
 class ECGRs
 {
 public:
-  /**
-  *  Default ctor
-  *  Default detection method 'PanTompkins'
-  *  NOTE: It is necessary to set filtered signal before start R peaks detection
-  */
+
   ECGRs ();
-  
-  /**
-  *  ctor
-  *  Default detection method 'PanTompkins'
-  *  Ctor starts R peaks detection automaticaly
-  *  @param filtered signal
-  */
-  ECGRs (IntSignal filteredSignal);
+  ~ECGRs ();
 
-  /**
-  *  ctor
-  *  Ctor starts R peaks detection automaticaly
-  *  @param filteredSignal filtered signal from 'ECG_BASALINE'
-  *  @param R_PEAKS_DETECTION_METHOD detection method
-  */
-  ECGRs (IntSignal filteredSignal, R_PEAKS_DETECTION_METHOD detectionMethod);
-
-  /**
-  *  Default destructor
-  */
-  ~ECGRs (void);
-
-  /**
-  *  @return vector with R peaks position
-  */
   IntSignal GetRs () const
   {
-    return rsPosition;
+    return rs;
   }
-  /**
-  *  Check R peaks detection status
-  *  @return true if detection is finisched
-  */
-  bool areRsDetected();
-
-  /**
-  *  Set new filtered signal
-  *  @param filteredSignal filtered signal
-  */
-  void setFilteredSignal(IntSignal filteredSignal);
-
-  /**
-  *  Get current filtered signal
-  *  @return filtered signal
-  */
-  IntSignal getFilteredSignal();
-
-  /**
-  *  Set new filtered method
-  *  @param detectionMethod detection method
-  */
-  void setDetectionMethod(R_PEAKS_DETECTION_METHOD detectionMethod);
-
-  /**
-  *  Get current filtered method
-  *  @return filtered method
-  */
-  R_PEAKS_DETECTION_METHOD getDetectionMethod();
-
-  /**
-  *  Execute R peaks detection
-  *  @return false if detection cannot be executed
-  *  @return true if detection is complete
-  */
-  bool detectRPeaks();
 
 private:
-  /**
-  *  Information about detection status
-  */
-  bool rsDetected;
 
-  /**
-  *  Vector with R peaks position
-  */
-  IntSignal rsPosition;
+  IntSignal rs;
 
-  /**
-  *  Filtered signal from 'ECG_BASALINE'
-  */
-  IntSignal filteredSignal;
-
-  /**
-  *  R peaks detection method
-  */
-  R_PEAKS_DETECTION_METHOD detectionMethod;
-
-  /**
-  *  PanTompkins R peaks method detection
-  *  @param pointer to ECG signal
-  */
-  void panTompkinsRPeaksDetection(IntSignal *signal);
-  
-  /**
-  *  Hilbert R peaks method detection
-  *  @param pointer to ECG signal
-  */
-  void hilbertRPeaksDetection(IntSignal *signal);
 };
 
