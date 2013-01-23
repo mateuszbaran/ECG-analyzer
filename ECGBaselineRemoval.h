19a4c2e6564945cf23dd04c87c172ce4852f6d18
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2013-01-23 06:26:16 +0100

    Sets up parameter settings for baseline - progress

--- ECGBaselineRemoval.h
+++ ECGBaselineRemoval.h
@@ -13,9 +13,14 @@ public:
 
 private:
 	BASELINE_REMOVAL_METHOD baselineRemovalMethod;
+	int span;
+	int order;
+	double cutoff_frequency;
+	double attenuation;
+	double ripple;
 
-	void chebyshevBaselineRemoval(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, const ECGInfo &ecgInfo, int cutoffFreq);
-	void butterworthBaselineRemoval(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, const ECGInfo &ecgInfo, int cutoffFreq);
+	void chebyshevBaselineRemoval(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, const ECGInfo &ecgInfo, int order, double cutoffFrequency, double attenuation);
+	void butterworthBaselineRemoval(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, const ECGInfo &ecgInfo, int order, double cutoffFrequency, double ripple);
 	void movingAverageBaselineRemoval(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, int span);
 	double calculateAvgValueOfNeighbours(gsl_vector *signal, int currentIndex, int span);
 };
