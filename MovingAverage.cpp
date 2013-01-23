66d958543c3a58962d4b95f1d1d256e4424b7f72
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2013-01-23 08:41:35 +0100

    Moves moving average to separate files; Fixes better channel evaluation

---
+++ MovingAverage.cpp
@@ -0,0 +1,34 @@
+#include "MovingAverage.h"
+
+void MovingAverage::removeBaseline(ECGSignalChannel &inputSignal, ECGSignalChannel &outputSignal, int span)
+{
+	auto signalLength = outputSignal-> signal -> size;
+
+	for(long index = 0; index < signalLength; index++)
+	{
+		auto edge = ceil((float)span/2);
+
+		if(index < edge || index > signalLength - edge) 
+		{
+			auto inputValue = gsl_vector_get (inputSignal -> signal, index);
+			gsl_vector_set(outputSignal -> signal, index, inputValue);
+		}
+		else
+		{
+			auto avgValue = this->calculateAvgValueOfNeighbours(inputSignal -> signal, index, span);		
+			gsl_vector_set (outputSignal -> signal, index, avgValue);
+		}
+	}
+}
+
+double MovingAverage::calculateAvgValueOfNeighbours(gsl_vector *signal, int currentIndex, int span)
+{
+	double sum = 0.00;
+	for(int i = 1; i <= span/2; i++)
+	{
+		sum += gsl_vector_get (signal, currentIndex - i);
+		sum += gsl_vector_get (signal, currentIndex + i);
+	}
+
+	return sum/span;
+}
