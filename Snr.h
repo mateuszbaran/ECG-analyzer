49dfa015ea5e124120ee427259c917684f9c43c2
Piotr Matuszkiewicz <piotr.matuszkiewicz@gmail.com>
2013-01-23 07:44:21 +0100

    Adds signal evaluation methods (snr, psnr, mse, rms)

--- Snr.h
+++ Snr.h
@@ -1,10 +1,18 @@
 #pragma once
+#include "ModulesMethods.h"
+#include "ModulesInterfaces.h"
 
 class Snr
 {
 	Snr(){};
 	~Snr(){};
 
-	double snr();
-	double psnr();
+	double snr(ECGSignalChannel &filtered_signal, ECGSignalChannel & raw_signal);
+	double psnr(ECGSignalChannel &filtered_signal, ECGSignalChannel & raw_signal);
+
+private:
+
+	double mse(ECGSignalChannel &filtered_signal, ECGSignalChannel & raw_signal);
+	double rms(ECGSignalChannel &signal);
+	void extract_noise(ECGSignalChannel &filtered_signal, ECGSignalChannel & raw_signal, ECGSignalChannel & noise);
 };
