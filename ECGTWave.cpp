5a168061f0521cb8a6edb264b4c48cee07226256
Mateusz Baran <mateuszbaran89@gmail.com>
2013-01-23 22:57:51 +0100

    t wave alt merged
    Copying new stuff instead of clicking merge is so much easier.

--- ECGTWave.cpp
+++ ECGTWave.cpp
@@ -1,5 +1,13 @@
 #include "ECGTWave.h"
 
-ECGTWave::ECGTWave (void) { }
+ECGTWave::ECGTWave (void) { 
+	numberOfWinDetected = 0;
+	percentageOfWinDetected = 0;
+}
 
 ECGTWave::~ECGTWave (void) { }
+
+void ECGTWave::setTWaveAlt(IntSignal twa)
+{
+	this->twa = twa;        
+}
