854148b196539464eac97d9e8030544ac600b749
Krzysztof Bębenek <krzysiek@e-bebenek.pl>
2013-01-27 20:02:53 +0100

    Poprawione uzupełnianie klasy QRSClass

--- QRSMorphologyDetector.cpp
+++ QRSMorphologyDetector.cpp
@@ -1,6 +1,7 @@
 #include "QRSMorphologyDetector.h"
 
 
+
 double dlugosc(ECGSignalChannel * signal, int forBegin, int forEnd){
 
 	ECGSignalChannel sig;
@@ -26,9 +27,12 @@ double pole(ECGSignalChannel * signal, int forBegin, int forEnd){
 	sig = *signal;
 
 	double result = 0;
+	double temp;
 
 	for(int i=forBegin;i<forEnd;i++){
-		result += gsl_vector_get(sig->signal,i);
+		temp = gsl_vector_get(sig->signal,i);
+		if (temp > 0 ) result += temp;
+		else result -= temp;
 	}
 
 	return result;
@@ -46,8 +50,25 @@ double stosunek(ECGSignalChannel * signal, int forBegin, int forEnd)
 		if(gsl_vector_get(sig->signal,i)>0) tmp1 += gsl_vector_get(sig->signal,i);
 		else tmp2 -= gsl_vector_get(sig->signal,i);
 	}
-
+	if (tmp2 == 0) return 0;
 	return tmp1/tmp2;
+}
+
+double szybkosc(ECGSignalChannel * signal, int forBegin, int forEnd)
+{
+	ECGSignalChannel sig;
+	sig = *signal;
+
+	double tmp1 = 0;
+	double tmp2;
+
+	for(int i=forBegin;i<forEnd-5;i++){
+		tmp2 = gsl_vector_get(sig->signal,i+5) - gsl_vector_get(sig->signal,i);
+		if (tmp2<0) tmp2 = -tmp2;
+		if(tmp2>tmp1) tmp1 = tmp2;
+	}
+
+	return tmp1;
 }
 
 QRSMorphologyDetector::QRSMorphologyDetector(void)
@@ -77,6 +98,7 @@ void QRSMorphologyDetector::runModule(const ECGWaves & waves, const ECGSignalCha
 
 bool QRSMorphologyDetector::detectQRSMorphology()
 {
+
 	auto signalSize  = filteredSignal->signal->size;
 	gsl_vector_int *QRS_onset = qrsPosition.GetQRS_onset()->signal;
 	gsl_vector_int *QRS_end = qrsPosition.GetQRS_end()->signal;
@@ -91,16 +113,18 @@ bool QRSMorphologyDetector::detectQRSMorphology()
 		int stop = gsl_vector_int_get(QRS_end,i);
 		
 		double poleV = pole(&filteredSignal,start,stop);
+
 		double dlugoscV = dlugosc(&filteredSignal,start,stop);
-		double rm1 = (dlugoscV/2*sqrt(3.14*poleV))-1;
+		double rm = (dlugoscV/2*sqrt(3.14*poleV))-1;
 		double stosunekV = stosunek(&filteredSignal,start,stop);
+		double szybkoscV = szybkosc(&filteredSignal,start,stop);
 
 		//dokonczyc ocene rodzaju pobudzenia
 
-		if (rm1>0.5 && rm1<1.5) gsl_vector_int_set(tmpSig->signal,i,VENTRICULUS);
-		else gsl_vector_int_set(tmpSig->signal,i,SUPRACENTRICULAR);
+		if (rm>65 && rm<80 && szybkoscV>0.075) gsl_vector_int_set(tmpSig->signal,i,SUPRACENTRICULAR);
+		else gsl_vector_int_set(tmpSig->signal,i,VENTRICULUS);
 
 	}
-
+	qrsMorphology->setQrsMorphology(tmpSig);
 	return 1;
 }
--- QRSMorphologyDetector.h
+++ QRSMorphologyDetector.h
@@ -6,7 +6,13 @@
 #include <sstream>
 
 #include <gsl/gsl_sort_vector.h>
-
+/*
+enum QRSType
+{
+  VENTRICULUS,
+  SUPRACENTRICULAR
+};
+*/
 using namespace std;
 
 
@@ -31,4 +37,5 @@ private:
 	friend double pole(ECGSignalChannel * signal, int forBegin, int forEnd);
 	friend double dlugosc(ECGSignalChannel * signal, int forBegin, int forEnd);
 	friend double stosunek(ECGSignalChannel * signal, int forBegin, int forEnd);
+	friend double szybkosc(ECGSignalChannel * signal, int forBegin, int forEnd);
 };
